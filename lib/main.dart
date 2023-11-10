ce2a5c792d656b82f0405520ee8acb64bc8673d7
xiaoliu19890 <xiaoliu19890@gmail.com>
2023-11-10 19:07:37 +0800

    fix version 4.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ void main() async {
         "bundleId": packageInfo.packageName,
         "version": packageInfo.version,
         "oauth_type": "android",
-        // "build_affcode": "kvXM",
+        // "build_affcode": "ZNyn",
       };
     } else {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
