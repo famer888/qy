9cc47ffaf3b457fe8c62627dfb3122803da6dddd
风间 <fjian16888@gmail.com>
2024-11-21 18:56:50 +0800

    fix bug

--- lib/ui_layer/screens/common_widgets/video_player/monitor_mv_player.dart
+++ lib/ui_layer/screens/common_widgets/video_player/monitor_mv_player.dart
@@ -149,9 +149,7 @@ class _MonitorMvPlayerState extends State<MonitorMvPlayer>
   Widget _optinalContent() {
     FlickControlManager? controlManager = flickManager?.flickControlManager;
     bool isMute = controlManager?.isMute ?? false;
-    if (kIsWeb && !_isWebListen) {
-      isMute = true;
-    }
+
     return Container(
       height: 40.w,
       padding: EdgeInsets.symmetric(horizontal: MyTheme.pagePadding),
