707b31a1452d932bfa6005b29e583be74951002c
jiangyi <>
2025-05-09 14:36:45 +0700

    replace api domains

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -16,12 +16,12 @@ class BuildConfig {
   /// 备用接口线路
   static List<String> apiLines = kIsWeb
       ? [
-          'https://api1.ckxqhet.com/api.php',
+          'https://api1.pchtkuj.xyz/api.php',
         ]
       : [
-          'https://api2.ckxqhet.com/api.php',
-          'https://api3.ckxqhet.com/api.php',
-          'https://api4.ckxqhet.com/api.php',
+          'https://api2.pchtkuj.xyz/api.php',
+          'https://api3.pchtkuj.xyz/api.php',
+          'https://api4.pchtkuj.xyz/api.php',
         ];
 
   /// 备用线路
