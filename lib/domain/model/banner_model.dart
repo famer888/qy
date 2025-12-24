3e374384d8fef0265699cd87ce0bcdacd317d986
jiangyi <>
2025-12-24 12:34:06 +0700

    fix bannerModel

--- lib/domain/model/banner_model.dart
+++ lib/domain/model/banner_model.dart
@@ -11,6 +11,7 @@ class BannerModel {
   final int? fId;
   final int reportId;
   final int? reportType;
+  final String? imgUrl;
   final String? urlStr;
   final int? adType;
   final String? adSlotName;
@@ -31,6 +32,7 @@ class BannerModel {
     required this.reportId,
     required this.reportType,
     required this.urlStr,
+    this.imgUrl,
     this.adType,
     this.adSlotName,
     this.advertiseCode,
@@ -39,18 +41,19 @@ class BannerModel {
 
   factory BannerModel.fromJson(Map<String, dynamic> json) => BannerModel(
         id: json['id'],
-        linkUrl: json['link_url'],
-        resourceUrl: json['resource_url'],
-        redirectType: json['redirect_type'],
+        linkUrl: json['link_url'] ?? '',
+        resourceUrl: json['resource_url'] ?? '',
+        redirectType: json['redirect_type'] ?? 0,
         title: json['title'],
         name: json['name'],
         desc: json['desc'],
         router: json['router'] ?? '',
         openType: json['open_type'],
         fId: json['f_id'],
-        reportId: json['report_id'],
-        reportType: json['report_type'],
+        reportId: json['report_id'] ?? 0,
+        reportType: json['report_type'] ?? 0,
         urlStr: json['url_str'] ?? '',
+        imgUrl: json['img_url'] ?? '',
         adType: json['ad_type'] is int
             ? json['ad_type']
             : int.tryParse('${json['ad_type']}'),
@@ -72,6 +75,7 @@ class BannerModel {
         'report_id': reportId,
         'report_type': reportType,
         'url_str': urlStr,
+        'img_url': imgUrl,
         'ad_type': adType,
         'ad_slot_name': adSlotName,
         'advertise_code': advertiseCode,
