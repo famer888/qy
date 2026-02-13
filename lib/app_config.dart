5e1a90c18824e4652c85341ad54358094cd4afff
Bumen <>
2026-02-13 16:35:21 +0700

    replace api domains

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -20,12 +20,12 @@ class BuildConfig {
   /// 备用接口线路
   static List<String> apiLines = kIsWeb
       ? [
-          'https://api1.tptsrhf.xyz/api.php',
+          'https://api1.ovrromzu.com/api.php',
         ]
       : [
-          'https://api2.tptsrhf.xyz/api.php',
-          'https://api3.tptsrhf.xyz/api.php',
-          'https://api4.tptsrhf.xyz/api.php',
+          'https://api2.ovrromzu.com/api.php',
+          'https://api3.ovrromzu.com/api.php',
+          'https://api4.ovrromzu.com/api.php',
         ];
 
   /// 备用线路
